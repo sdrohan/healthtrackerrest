d02c8f55e39c15a145ae28bcabd9ed75f3472d9f
Siobhan Drohan <sdrohan@wit.ie>
2023-06-12 16:55:13 +0100

    Lab05 - Get Remote Assigned Port

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -9,12 +9,19 @@ class JavalinConfig {
     fun startJavalinService(): Javalin {
 
         val app = Javalin.create().apply {
-            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-            error(404) { ctx -> ctx.json("404 - Not Found") }
-        }.start(7000)
+            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
+            error(404) { ctx -> ctx.json("404 : Not Found") }
+        }.start(getRemoteAssignedPort())
 
         registerRoutes(app)
         return app
+    }
+
+    private fun getRemoteAssignedPort(): Int {
+        val remotePort = System.getenv("PORT")
+        return if (remotePort != null) {
+            Integer.parseInt(remotePort)
+        } else 7000
     }
 
     private fun registerRoutes(app: Javalin) {
