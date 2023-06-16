3035be18ae590a64356ad78a538520b62a46a360
Siobhan Drohan <sdrohan@wit.ie>
2023-06-16 17:18:03 +0100

    Lab07 - Before starting exercises

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -3,11 +3,11 @@ package ie.setu.controllers
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.joda.JodaModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import ie.setu.domain.Activity
 import ie.setu.domain.User
 import ie.setu.domain.repository.ActivityDAO
 import ie.setu.domain.repository.UserDAO
+import ie.setu.utils.jsonToObject
 import io.javalin.http.Context
 
 object HealthTrackerController {
@@ -16,42 +16,64 @@ object HealthTrackerController {
     private val activityDAO = ActivityDAO()
 
     fun getAllUsers(ctx: Context) {
-        ctx.json(userDao.getAll())
+        val users = userDao.getAll()
+        if (users.size != 0) {
+            ctx.status(200)
+        }
+        else{
+            ctx.status(404)
+        }
+        ctx.json(users)
     }
 
     fun getUserByUserId(ctx: Context) {
         val user = userDao.findById(ctx.pathParam("user-id").toInt())
         if (user != null) {
             ctx.json(user)
+            ctx.status(200)
+        }
+        else{
+            ctx.status(404)
         }
     }
 
     fun addUser(ctx: Context) {
-        val mapper = jacksonObjectMapper()
-        val user = mapper.readValue<User>(ctx.body())
-        userDao.save(user)
-        ctx.json(user)
+        val user : User = jsonToObject(ctx.body())
+        val userId = userDao.save(user)
+        if (userId != null) {
+            user.id = userId
+            ctx.json(user)
+            ctx.status(201)
+        }
     }
 
     fun getUserByEmail(ctx: Context) {
         val user = userDao.findByEmail(ctx.pathParam("email"))
         if (user != null) {
             ctx.json(user)
+            ctx.status(200)
+        }
+        else{
+            ctx.status(404)
         }
     }
 
-    fun deleteUser(ctx: Context) {
-        userDao.delete(ctx.pathParam("user-id").toInt())
+    fun deleteUser(ctx: Context){
+        if (userDao.delete(ctx.pathParam("user-id").toInt()) != 0)
+            ctx.status(204)
+        else
+            ctx.status(404)
     }
 
-    fun updateUser(ctx: Context) {
-        val mapper = jacksonObjectMapper()
-        val userUpdates = mapper.readValue<User>(ctx.body())
-        userDao.update(
-            id = ctx.pathParam("user-id").toInt(),
-            user = userUpdates
-        )
+    fun updateUser(ctx: Context){
+        val foundUser : User = jsonToObject(ctx.body())
+        if ((userDao.update(id = ctx.pathParam("user-id").toInt(), user=foundUser)) != 0)
+            ctx.status(204)
+        else
+            ctx.status(404)
     }
+
+
 
     //--------------------------------------------------------------
     // ActivityDAOI specifics
@@ -80,10 +102,7 @@ object HealthTrackerController {
 
     fun addActivity(ctx: Context) {
         //mapper handles the serialisation of Joda date into a String.
-        val mapper = jacksonObjectMapper()
-            .registerModule(JodaModule())
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        val activity = mapper.readValue<Activity>(ctx.body())
+        val activity : Activity = jsonToObject(ctx.body())
         activityDAO.save(activity)
         ctx.json(activity)
     }
@@ -107,10 +126,7 @@ object HealthTrackerController {
     }
 
     fun updateActivity(ctx: Context){
-        val mapper = jacksonObjectMapper()
-            .registerModule(JodaModule())
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        val activity = mapper.readValue<Activity>(ctx.body())
+        val activity : Activity = jsonToObject(ctx.body())
         activityDAO.updateByActivityId(
             activityId = ctx.pathParam("activity-id").toInt(),
             activityDTO=activity)
--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -26,12 +26,12 @@ class UserDAO {
         }
     }
 
-    fun save(user: User){
-        transaction {
+    fun save(user: User) : Int?{
+        return transaction {
             Users.insert {
                 it[name] = user.name
                 it[email] = user.email
-            }
+            } get Users.id
         }
     }
 
@@ -52,8 +52,8 @@ class UserDAO {
         }
     }
 
-    fun update(id: Int, user: User){
-        transaction {
+    fun update(id: Int, user: User): Int{
+        return transaction {
             Users.update ({
                 Users.id eq id}) {
                 it[name] = user.name
